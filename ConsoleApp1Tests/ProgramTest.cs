f9537d1721955b3b4ada78dc6170610ef3f721b0
minaammunir <minaammunir@gmail.com>
2019-07-15 04:03:04 +0500

    initial commit

---
+++ ConsoleApp1Tests/ProgramTest.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp1;
+using NUnit.Framework;
+using Shouldly;
+
+namespace ConsoleApp1Tests
+{
+    /// <summary>
+    /// Summary description for ProgramTest
+    /// </summary>
+    [TestFixture]
+    public class ProgramTest
+    {
+        [Test]
+        public void HelloMessage_WhenCalled_ReturnsHello()
+        {
+            // Act
+            var result = Program.HelloMessage();
+
+            // Assert
+            result.ShouldBe("Hello");
+        }
+    }
+}
---
+++ ConsoleApp1Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConsoleApp1Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp1Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4fdff4a7-f6a4-4e94-b564-855d2295b394")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
